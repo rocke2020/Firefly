459426a1d008f6055bde89fd9be7fc3d156232f2
qichang.dong <qcdong@metanovas.com>
2023-11-12 11:16:17 +0800

    u

--- data_process/postprocess/move_tfevents_to_vscode.py
+++ data_process/postprocess/move_tfevents_to_vscode.py
@@ -13,15 +13,21 @@ from comm_utils.log_util import ic, logger
 
 root_out = Path('/mnt/nas1/models/llama/quantized_models')
 TASK_NAME_PREFIX = 'llama2-7b-ner-chem_gene'
-events_dir = Path(f'runs/{TASK_NAME_PREFIX}')
-events_dir.mkdir(parents=True, exist_ok=True)
+out_events_dir = Path(f'runs/{TASK_NAME_PREFIX}')
+out_events_dir.mkdir(parents=True, exist_ok=True)
 
 for path in root_out.iterdir():
     if path.name.startswith(TASK_NAME_PREFIX) and path.is_dir():
         run_dir = path / 'runs'
         if not run_dir.exists():
             continue
-        for run in run_dir.iterdir():
-            out_dir = events_dir / run.name
-            shutil.copytree(run, out_dir, dirs_exist_ok=True, symlinks=True)
-            
+        event_dir = list(run_dir.iterdir())[0]
+        out_dir = out_events_dir / event_dir.name
+        shutil.copytree(event_dir, out_dir, dirs_exist_ok=True, symlinks=True)
+        readme_file = out_dir / f'{path.name}.md'
+        with open(readme_file, 'w', encoding='utf-8') as f:
+            f.write(f'# {path.name}\n')
+        for file in path.iterdir():
+            if file.name.endswith(('.log', '.json')):
+                out_file = out_dir / file.name
+                os.symlink(file, out_file)
